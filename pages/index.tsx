033725ce2c7485550578cba2bebdc5920b0c13ac
Haruna Popoola <haruna.popoola@yahoo.com>
2022-04-09 15:57:36 +0100

    feature: add tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return <div className={styles.container} />
+  return (<h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>);
 }
 
 export default Home
